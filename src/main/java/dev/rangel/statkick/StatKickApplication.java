363a99c1bd7449129e27abfcf6ea566542a7b076
Rangel <joaquimrangel.dev@gmail.com>
2026-09-22 16:32:53 -0300

    chore: setup initial Spring Boot project with Maven and Java 21

---
+++ src/main/java/dev/rangel/statkick/StatKickApplication.java
@@ -0,0 +1,13 @@
+package dev.rangel.statkick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatKickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatKickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rangel/statkick/StatKickApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rangel.statkick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatKickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
